c7870dd4afed4452d46061222581b01a6293ce8b
Lauren (Laurie) Samuels <lauren.samuels@vanderbilt.edu>
2017-08-05 16:06:26 -0500

    1 small change

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ shinyUI(fluidPage(
     sidebarLayout(
         sidebarPanel(
             sliderInput("propTx",
-                "Proportion treated:",
+                "Proportiont treated:",
                 min = 0,
                 max = 1,
                 value = 0.5
